08bead9401caacf2cb65a744d4b38b761ed60fc7
dfldylan <dfldylan@qq.com>
2024-01-23 09:06:31 +0800

    fix(density): track_feats can densify and prune

--- scene/trans_model.py
+++ scene/trans_model.py
@@ -46,7 +46,7 @@ class TransModel:
         self.feats = optimizable_tensors["feats"]
 
     def densify(self, selected_pts_mask, N=1):
-        print("add {} points".format(torch.sum(selected_pts_mask).cpu().detach().numpy()))
+        print("add {} points".format(2 * torch.sum(selected_pts_mask).cpu().detach().numpy()))
         feats = self.feats[selected_pts_mask].repeat(N, 1)
         self.feats = self.cat_tensors_to_optimizer({'feats': feats})['feats']
 
