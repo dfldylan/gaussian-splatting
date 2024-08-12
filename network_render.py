a9e81ad61aff79660c9e085291759dab5cadca56
dylan <dfldylan@qq.com>
2024-08-12 23:43:40 +0800

    support prune in network_render.py

--- network_render.py
+++ network_render.py
@@ -1,15 +1,15 @@
 import os
 import time
-
+import copy
 import torch
 from gaussian_renderer import render, network_gui
 import sys
 from scene import Scene, GaussianModel
 from argparse import ArgumentParser
-from arguments import ModelParams, PipelineParams
+from arguments import ModelParams, PipelineParams, OptimizationParams
 
 
-def pipeline(dataset, pipe):
+def pipeline(dataset, opt, pipe):
     path = dataset.model_path
 
     bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
@@ -21,10 +21,13 @@ def pipeline(dataset, pipe):
     for folder in folders:
         gaussians = GaussianModel(dataset.sh_degree)
         gaussians.load_ply(os.path.join(path, folder, 'point_cloud', 'iteration_30000', 'point_cloud.ply'))
+        gaussians.training_setup(opt)
         gaussians_list.append(gaussians)
 
     print('Load gaussians success!')
     # pipeline
+
+    mask = None
 
     while True:
         while network_gui.conn == None:
@@ -36,8 +39,11 @@ def pipeline(dataset, pipe):
              scaling_modifer, frame) = network_gui.receive()
             if custom_cam != None:
                 frame = min(frame, len(gaussians_list) - 1)
-                net_image = render(custom_cam, gaussians_list[frame], pipe, background,
-                                   scaling_modifer)["render"]
+                gaussians_frame: GaussianModel = gaussians_list[frame]
+                if mask is not None:
+                    gaussians_frame = copy.deepcopy(gaussians_frame)
+                    gaussians_frame.prune_points(mask)
+                net_image = render(custom_cam, gaussians_frame, pipe, background, scaling_modifer)["render"]
                 net_image_bytes = memoryview((torch.clamp(net_image, min=0, max=1.0) * 255).byte()
                                              .permute(1, 2, 0).contiguous().cpu().numpy())
             network_gui.send(net_image_bytes, dataset.source_path)
@@ -49,6 +55,7 @@ if __name__ == "__main__":
     # Set up command line argument parser
     parser = ArgumentParser(description="Training script parameters")
     lp = ModelParams(parser)
+    op = OptimizationParams(parser)
     pp = PipelineParams(parser)
     parser.add_argument('--ip', type=str, default="0.0.0.0")
     parser.add_argument('--port', type=int, default=6009)
@@ -56,4 +63,4 @@ if __name__ == "__main__":
 
     # Start GUI server, configure and run training
     network_gui.init(args.ip, args.port)
-    pipeline(lp.extract(args), pp.extract(args))
+    pipeline(lp.extract(args), op.extract(args), pp.extract(args))
