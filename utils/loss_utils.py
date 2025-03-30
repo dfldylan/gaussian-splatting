370d3384f5e0520d7924c59efe81921331b641e3
dylan <dfldylan@qq.com>
2025-03-30 12:34:02 +0800

    fix: l1_loss with mask

--- utils/loss_utils.py
+++ utils/loss_utils.py
@@ -19,7 +19,7 @@ from utils.density import compute_density
 def l1_loss(network_output, gt, mask=None):
     if mask is not None and mask.sum() > 0:
         loss = torch.abs(network_output - gt) * mask
-        return loss.sum() / mask.sum()
+        return loss.mean(0).sum() / mask.sum()
     return torch.abs(network_output - gt).mean()
 
 def l2_loss(network_output, gt):
